93dd3ee99b8aa23379a428da0205982a25823e08
Dionysis Zindros <dionyziz@gmail.com>
2012-11-05 17:53:32 -0500

    Add playfield rotation

--- color.js
+++
@@ -1,6 +0,0 @@
-function rgbRandom() {
-    function randomVal() {
-        return Math.floor( 1000 * Math.random() ) % 256;
-    }
-    return 'rgb( ' + randomVal() + ', ' + randomVal() + ', ' + randomVal() + ' )';
-}
--- simulation.js
+++
@@ -1,6 +0,0 @@
-var W = 600, H = 600;
-var BALL_RADIUS = 20;
-var GRAVITY = new Vector( 0, 5 );
-var DAMPING = 1 / Math.sqrt( 2 );
-var FRICTION = 0.99;
-var balls = [];
